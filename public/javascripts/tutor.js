fe1a86b7b27a2b3de09543cc04ecf512727a5f80
andrey.vokin <andrey.vokin@jetbrains.com>
2011-08-04 17:27:14 +0400

    Hotkey navigation changed

--- public/javascripts/tutor.js
+++ public/javascripts/tutor.js
@@ -3,34 +3,42 @@ function addTranslation() {
   var createdInput = document.createElement("input");
   translationsCount++;
   createdInput.name = "translation_" + (translationsCount - 1);
-  createdInput.onkeypress = onKeyPressed;
+  createdInput.id = createdInput.name;
+  createdInput.setAttribute("data-autocomplete", "/search/autocomplete_word_word");
+  createdInput.onkeydown = onKeyPressed;
   translations.appendChild(createdInput);
   createdInput.focus();
 }
 
 function addCategory() {
-  var translations = document.getElementById("category");
+  var categories = document.getElementById("categories");
   var createdInput = document.createElement("input");
   categoriesCount++;
   createdInput.name = "category_" + (categoriesCount - 1);
-  createdInput.onkeypress = onKeyPressed;
+  createdInput.id = createdInput.name;
   createdInput.setAttribute("data-autocomplete", "/search/autocomplete_category_name");
+  createdInput.onkeydown = onKeyPressed;
   categories.appendChild(createdInput);
   createdInput.focus();
 }
 
-function onKeyPressed() {
-  if (event.keyCode != null) {
-    if (event.keyCode == 9) {
-      if (event.target.id.substring(0, 11) == "translation") {
-        addTranslation();
-      } else {
-        addCategory();
-      }
+function test11() {
+  alert("test")
+}
 
-      event.preventDefault();
-      return false;
+function onKeyPressed() {
+  if (!event.ctrlKey) {
+    return true;
+  }
+  if (event.keyCode == 81) {
+    if (event.target.id.substring(0, 11) == "translation") {
+      addTranslation();
+    } else {
+      addCategory();
     }
+
+    event.preventDefault();
+    return false;
   }
   return true;
 }
