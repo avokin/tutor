9305f1778a468723cf6cac20e8aa5cc98ab1c6a1
Andrey Vokin <andrey.vokin@jetbrains.com>
2011-11-05 16:21:51 +0400

    Fixed issues: hotkey fixed (s for search)

--- public/javascripts/tutor.js
+++ public/javascripts/tutor.js
@@ -1,7 +1,8 @@
 function documentKeypress(e) {
   if (e.ctrlKey) {
+    // i
     if (e.keyCode == 9) {
-      var wordToSearch = document.getElementById("word_to_search");
+      var wordToSearch = document.getElementById("search_word");
       wordToSearch.focus();
     } else if (e.keyCode == 17) {
       if (onCtrlQ != null) {
@@ -15,6 +16,13 @@ function documentKeypress(e) {
       if (onCtrlB != null) {
         onCtrlB();
       }
+    }
+    // s or S
+  } else if (e.keyCode == 115 || e.keyCode == 83) {
+    if (e.srcElement.type != "text") {
+      var wordToSearch = document.getElementById("search_word");
+      wordToSearch.focus();
+      return false;
     }
   }
 }
