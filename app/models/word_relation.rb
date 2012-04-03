05980ff25b880dcf8a5b914f8060d5461c2c6af7
andrey.vokin <andrey.vokin@jetbrains.com>
2012-04-03 19:13:33 +0400

    Removed success_count from word_relation type

--- app/models/word_relation.rb
+++ app/models/word_relation.rb
@@ -1,5 +1,5 @@
 class WordRelation < ActiveRecord::Base
-  attr_accessible :status_id, :success_count, :related_user_word_id, :relation_type
+  attr_accessible :status_id, :related_user_word_id, :relation_type
 
   belongs_to :source_user_word, :class_name => 'UserWord', :foreign_key => :source_user_word_id
   belongs_to :related_user_word,  :class_name => 'UserWord', :foreign_key => :related_user_word_id
@@ -9,7 +9,6 @@ class WordRelation < ActiveRecord::Base
   validates :source_user_word, :presence => true
   validates :related_user_word, :presence => true
   validates :status_id, :presence => true
-  validates :success_count, :presence => true
   validates :user, :presence => true
 
   validates_uniqueness_of :relation_type, :scope => [:source_user_word_id, :related_user_word_id]
@@ -30,14 +29,14 @@ class WordRelation < ActiveRecord::Base
         if related_user_word.word.language_id == user_word.word.language_id
           return nil
         end
-        word_relation = user_word.direct_translations.build(:status_id => 1, :success_count => 0)
+        word_relation = user_word.direct_translations.build(:status_id => 1)
         word_relation.relation_type = 1
       when "2"
         if related_user_word.word.language_id != user_word.word.language_id || related_user_word == user_word
           return nil
         end
 
-        word_relation = user_word.direct_synonyms.build(:status_id => 1, :success_count => 0)
+        word_relation = user_word.direct_synonyms.build(:status_id => 1)
         word_relation.relation_type = 2
       else
         word_relation = nil
@@ -52,28 +51,6 @@ class WordRelation < ActiveRecord::Base
       end
     end
     word_relation
-  end
-
-  def check(answer)
-    if answer == related_user_word.word.text
-      self.success_count += 1
-      if (self.success_count == 5)
-        self.status_id = 2
-      end
-      save!
-      return :right_answer
-    else
-      translation_list = WordRelation.find_all_translation(user, source_user_word.word.text)
-      translation_list.each do |translation|
-        if translation.related_user_word.word.text == answer
-          return :another_word
-        end
-      end
-      self.success_count = 0
-      self.status_id = 1
-      save!
-    end
-    :wrong_answer
   end
 
   def self.find_all_translation(user, text)
