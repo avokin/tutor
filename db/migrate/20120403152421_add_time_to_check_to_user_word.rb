4dc5c66e4e9ea3912b310764aef46ed1f1a9546d
andrey.vokin <andrey.vokin@jetbrains.com>
2012-04-18 10:33:04 +0400

    DateTime default value fix

--- db/migrate/20120403152421_add_time_to_check_to_user_word.rb
+++ db/migrate/20120403152421_add_time_to_check_to_user_word.rb
@@ -1,6 +1,6 @@
 class AddTimeToCheckToUserWord < ActiveRecord::Migration
   def change
-    add_column :user_words, :time_to_check, :datetime, :default => 0, :null => false
+    add_column :user_words, :time_to_check, :datetime, :default => DateTime.now, :null => false
     remove_column :word_relations, :success_count
   end
 end
