29edd7b2aea259cff8b792779368ae3a35e1257a
andrey.vokin <andrey.vokin@jetbrains.com>
2012-07-02 09:50:41 +0400

    Status of the word fixed

--- app/helpers/user_words_helper.rb
+++ app/helpers/user_words_helper.rb
@@ -13,4 +13,12 @@ module UserWordsHelper
       0
     end
   end
+
+  def get_training_status(user_word)
+    if user_word.translation_success_count < UserWord::TIME_LAPSES.length - 1
+      100 * user_word.translation_success_count / (UserWord::TIME_LAPSES.length - 1)
+    else
+      100
+    end
+  end
 end
