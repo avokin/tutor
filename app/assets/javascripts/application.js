bad576f149397bdf3c6d48a8eaaeaa4f24055ffa
andrey.vokin <andrey.vokin@jetbrains.com>
2013-01-30 13:57:47 +0400

    All vender files moved to corresponded "vendor" subdirectory

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,9 @@
-//= require jquery
-//= require jquery_ujs
+//= require /jquery
+//= require /jquery_ujs
 //= require twitter/bootstrap
 //= require_self
 //= require_tree .
 //= require vendor
-//= require autocomplete-rails
+//= require /autocomplete-rails
 
 /* rest of file omitted */
