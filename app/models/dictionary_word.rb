ffc4efc6addd35648696e5cdd2468fc621ae8e62
andrey.vokin <andrey.vokin@jetbrains.com>
2013-03-30 18:43:43 +0400

    Transcription database configuration

--- app/models/dictionary_word.rb
+++ app/models/dictionary_word.rb
@@ -1,3 +1,9 @@
 class DictionaryWord < ActiveRecord::Base
-  establish_connection ENV['HEROKU_POSTGRESQL_VIOLET_URL']
+  if Rails.env == "production"
+    dictionary_db = ENV['HEROKU_POSTGRESQL_VIOLET_URL']
+  else
+    dictionary_db = :dictionary_database
+  end
+
+  establish_connection dictionary_db
 end
